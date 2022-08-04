4aba760151bfc4e0dfaa0c083e3a686ad9c1d87c
Denis Rystsov <denis@vectorized.io>
2022-08-04 13:52:43 -0700

    fix smoke

--- driver-redpanda/deploy/certification/suites/suite-smoke-all.sh
+++ driver-redpanda/deploy/certification/suites/suite-smoke-all.sh
@@ -8,18 +8,18 @@ sudo rm -rf footprint.tar.bz2
 sudo rm -rf *.json
 
 retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/load.625k.yaml
-#retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/load.900k.yaml
-#retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/load.1250k.yaml
-#
-#retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-true.yaml $OMB/workloads/release/smoke/load.625k.yaml
-#retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-1-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/load.625k.yaml
-#
-#retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-1-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/simple.yaml
-#retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/simple.yaml
-#retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-true.yaml $OMB/workloads/release/smoke/simple.yaml
-#
-#retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-1-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/e2e.yaml
-#retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/e2e.yaml
-#retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-true.yaml $OMB/workloads/release/smoke/e2e.yaml
+retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/load.900k.yaml
+retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/load.1250k.yaml
+
+retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-true.yaml $OMB/workloads/release/smoke/load.625k.yaml
+retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-1-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/load.625k.yaml
+
+retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-1-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/simple.yaml
+retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/simple.yaml
+retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-true.yaml $OMB/workloads/release/smoke/simple.yaml
+
+retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-1-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/e2e.yaml
+retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-false.yaml $OMB/workloads/release/smoke/e2e.yaml
+retry-on-error sudo $OMB/bin/benchmark -t swarm -d $OMB/driver-redpanda/redpanda-ack-all-linger-1ms-eod-true.yaml $OMB/workloads/release/smoke/e2e.yaml
 
 sudo tar cjf footprint.tar.bz2 $(ls *json)
