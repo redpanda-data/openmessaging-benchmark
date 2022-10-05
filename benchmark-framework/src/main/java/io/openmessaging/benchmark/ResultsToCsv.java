d15b73e5105112a6986087d28730cd624cc7796e
tmgstevens <tristan@apache.org>
2022-10-05 16:56:09 +0100

    CSV Improvements to improve parameter handling and ensure that full set of results are written to CSV

--- benchmark-framework/src/main/java/io/openmessaging/benchmark/ResultsToCsv.java
+++ benchmark-framework/src/main/java/io/openmessaging/benchmark/ResultsToCsv.java
@@ -16,7 +16,6 @@ package io.openmessaging.benchmark;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.HdrHistogram.Histogram;
-import org.bouncycastle.util.test.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,16 +32,25 @@ public class ResultsToCsv {
 
     private static final Logger log = LoggerFactory.getLogger(ResultsToCsv.class);
 
-    public void writeAllResultFiles(String directory) {
+    public void writeAllResultFiles(File directory) {
         try {
-            File dir = new File(directory);
-            File[] directoryListing = dir.listFiles();
+            File[] directoryListing = directory.listFiles();
             Arrays.sort(directoryListing);
 
             List<String> lines = new ArrayList<>();
-            lines.add("topics,partitions,message-size,producers-per-topic,consumers-per-topic," +
+            lines.add("workload,driver,version,beginTime,endTime,topics,partitions," +
+                    "message-size,producers-per-topic,consumers-per-topic," +
                     "prod-rate-min,prod-rate-avg,prod-rate-std-dev,prod-rate-max," +
-                    "con-rate-min,con-rate-avg,con-rate-std-dev,con-rate-max,");
+                    "con-rate-min,con-rate-avg,con-rate-std-dev,con-rate-max," +
+                    "aggregatedPublishLatencyAvg,aggregatedPublishLatency50pct,aggregatedPublishLatency75pct," +
+                    "aggregatedPublishLatency95pct,aggregatedPublishLatency99pct,aggregatedPublishLatency999pct," +
+                    "aggregatedPublishLatency9999pct,aggregatedPublishLatencyMax,aggregatedPublishDelayLatencyAvg," +
+                    "aggregatedPublishDelayLatency50pct,aggregatedPublishDelayLatency75pct," +
+                    "aggregatedPublishDelayLatency95pct,aggregatedPublishDelayLatency99pct," +
+                    "aggregatedPublishDelayLatency999pct,aggregatedPublishDelayLatency9999pct," +
+                    "aggregatedPublishDelayLatencyMax,aggregatedEndToEndLatencyAvg,aggregatedEndToEndLatency50pct," +
+                    "aggregatedEndToEndLatency75pct,aggregatedEndToEndLatency95pct,aggregatedEndToEndLatency99pct," +
+                    "aggregatedEndToEndLatency999pct,aggregatedEndToEndLatency9999pct,aggregatedEndToEndLatencyMax");
 
             List<TestResult> results = new ArrayList<>();
             for (File file : directoryListing) {
@@ -61,13 +69,13 @@ public class ResultsToCsv {
                 lines.add(extractResults(tr));
             }
 
-            String resultsFileName = "results-" + Instant.now().getEpochSecond() + ".csv";
-            FileWriter writer = new FileWriter(resultsFileName);
+            File csvFile = new File(directory, "results-" + Instant.now().getEpochSecond() + ".csv");
+            FileWriter writer = new FileWriter(csvFile);
             for (String str : lines) {
                 writer.write(str + System.lineSeparator());
             }
             writer.close();
-            log.info("Results extracted into CSV " + resultsFileName);
+            log.info("Results extracted into CSV " + csvFile.getAbsolutePath());
         }
         catch(Exception e) {
             log.error("Failed creating csv file.", e);
@@ -87,9 +95,21 @@ public class ResultsToCsv {
                 conRateHistogram.recordValueWithCount(rate.longValue(), 2);
             }
 
-            String line = MessageFormat.format("{0,number,#},{1,number,#},{2,number,#},{3,number,#},{4,number,#}," +
-                            "{5,number,#},{6,number,#},{7,number,#.##},{8,number,#}," +
-                            "{9,number,#},{10,number,#},{11,number,#.##},{12,number,#}",
+            String line = MessageFormat.format("{0},{1},{2},{3},{4},{5,number,#}," +
+                            "{6,number,#},{7,number,#},{8,number,#},{9,number,#}," +
+                            "{10,number,#},{11,number,#},{12,number,#.##},{13,number,#}," +
+                            "{14,number,#},{15,number,#},{16,number,#.##},{17,number,#}," +
+                            "{18,number,#.###},{19,number,#.###},{20,number,#.###},{21,number,#.###}," +
+                            "{22,number,#.###},{23,number,#.###},{24,number,#.###},{25,number,#.###}," +
+                            "{26,number,#.###},{27,number,#.###},{28,number,#.###},{29,number,#.###}," +
+                            "{30,number,#.###},{31,number,#.###},{32,number,#.###},{33,number,#.###}," +
+                            "{34,number,#.###},{35,number,#.###},{36,number,#.###},{37,number,#.###}," +
+                            "{38,number,#.###},{39,number,#.###},{40,number,#.###},{41,number,#.###}",
+                    tr.workload,
+                    tr.driver,
+                    tr.version,
+                    tr.beginTime,
+                    tr.endTime,
                     tr.topics,
                     tr.partitions,
                     tr.messageSize,
@@ -102,7 +122,32 @@ public class ResultsToCsv {
                     conRateHistogram.getMinNonZeroValue(),
                     conRateHistogram.getMean(),
                     conRateHistogram.getStdDeviation(),
-                    conRateHistogram.getMaxValue());
+                    conRateHistogram.getMaxValue(),
+                    tr.aggregatedPublishLatencyAvg,
+                    tr.aggregatedPublishLatency50pct,
+                    tr.aggregatedPublishLatency75pct,
+                    tr.aggregatedPublishLatency95pct,
+                    tr.aggregatedPublishLatency99pct,
+                    tr.aggregatedPublishLatency999pct,
+                    tr.aggregatedPublishLatency9999pct,
+                    tr.aggregatedPublishLatencyMax,
+                    tr.aggregatedPublishDelayLatencyAvg,
+                    tr.aggregatedPublishDelayLatency50pct,
+                    tr.aggregatedPublishDelayLatency75pct,
+                    tr.aggregatedPublishDelayLatency95pct,
+                    tr.aggregatedPublishDelayLatency99pct,
+                    tr.aggregatedPublishDelayLatency999pct,
+                    tr.aggregatedPublishDelayLatency9999pct,
+                    tr.aggregatedPublishDelayLatencyMax,
+                    tr.aggregatedEndToEndLatencyAvg,
+                    tr.aggregatedEndToEndLatency50pct,
+                    tr.aggregatedEndToEndLatency75pct,
+                    tr.aggregatedEndToEndLatency95pct,
+                    tr.aggregatedEndToEndLatency99pct,
+                    tr.aggregatedEndToEndLatency999pct,
+                    tr.aggregatedEndToEndLatency9999pct,
+                    tr.aggregatedEndToEndLatencyMax
+                    );
 
             return line;
         }
