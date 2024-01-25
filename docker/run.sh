a58409fc3f7e48232f3d6b62536ef4cde8dd0beb
Dave Voutila <voutilad@gmail.com>
2024-01-25 08:49:38 -0500

    Refactor the Docker image and how the benchmark scripts are invoked (#94)
    Changes the base Docker image to contain the "run.sh" script for kicking
    off a benchmark.
    
    Simplifies the entrypoints for the k8s pods. Instead of an endless tail
    of /dev/null, requests a tty and stdin so `kubectl attach` can be used.
    
    Addresses various issues seen in an AKS environment related to finding
    the Java installation:
    
    * Simplify commands and entrypoints to minimize path issues.
    * use JAVA_HOME to find java
    * Optimize image layer sizes. Fix execute bit on run.sh.
    * Add more details on running OMB via Helm.
    * Define JAVA_EXE based on JAVA_HOME, if present.

---
+++ docker/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+################################################################
+# Start a new benchmark run.
+################################################################
+
+TEST_NAME="/run/omb/test-$(date '+%Y-%m-%dT%H%M%S').json"
+DRIVER="${DRIVER:-/etc/omb/driver.yaml}"
+WORKLOAD="${WORKLOAD:-/etc/omb/workload.yaml}"
+
+if [ -z "${WORKERS}" ]; then
+  echo "WORKERS environment variable not set!" > /dev/stderr;
+  echo "Check your environment and set it to the list of worker urls." > /dev/stderr;
+  exit 1;
+fi
+
+/opt/benchmark/bin/benchmark \
+  --output "${TEST_NAME}" \
+  --drivers "${DRIVER}" \
+  --workers "${WORKERS}" \
+  "${WORKLOAD}" \
+  $*
